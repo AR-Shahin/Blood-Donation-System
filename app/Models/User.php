6bd93e04e4eede23f427bfdbf903d5e5baecc39c
AR-Shahin <mdshahinmije96@gmail.com>
2022-08-14 22:18:07 +0600

    frontend page design

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,52 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    // public function sendPasswordResetNotification($token)
+    // {
+    //     $this->notify(new AdminResetPasswordNotification($token));
+    // }
+
+    // public function sendPasswordResetNotification($token)
+    // {
+    //     $this->notify(new AdminResetPasswordNotification($token));
+    // }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,67 @@
 
 
 @section('app_content')
-
-    <h1 class="text-center my-5">Laravel 8 Template</h1>
-    <hr>
-    <div class="text-center">
-        <a href="{{ route('admin.login') }}" class="btn btn-sm btn-success"><i class="fa fa-user mr-1"></i> Admin Login</a>
-        {{ greetings() }}
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">BloodDonor</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup" ">
+        <div class="navbar-nav" >
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </div>
+      </div>
     </div>
+  </nav>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-4 text-center">
+            <div class="card">
+                <div class="card-body">
+                    <p>Register to save people's lives
+                        রক্তদাতা হতে রেজিস্ট্রেশন করুন</p>
+                    <a href="" class="btn btn-sm btn-success">Be a Donor</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 text-center">
+            <div class="card">
+                <div class="card-body">
+                    <p>Register to save people's lives
+                        রক্তদাতা হতে রেজিস্ট্রেশন করুন</p>
+                    <a href="" class="btn btn-sm btn-info">Be a Consumer</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table-bordered table text-center">
+                        <tr>
+                            <th>SL</th>
+                            <th>Blood Group</th>
+                            <th>Available Donor</th>
+                        </tr>
+                        @foreach (\App\Models\Blood::get() as $blood)
+                        <tr>
+                            <td>{{ $loop->index + 1 }}</td>
+                            <td>{{ $blood->name }}</td>
+                            <td>{{ rand(20,30) }}</td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
 @stop
